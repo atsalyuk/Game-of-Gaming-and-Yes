6302abca98955b0d73e10be9b9a59ea7d7ce45e8
Qisgreat <student@cch-w529-imac02.ccsd.d5>
2013-10-23 15:19:14 -0600

    aroghdalsh
    asdjhgkjasgdakd

---
+++ Game/src/game/Game.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package game;
+
+/**
+ *
+ * @author student
+ */
+public class Game {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
